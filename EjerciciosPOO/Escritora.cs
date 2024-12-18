a2964473d6e1a15f9c4cdb952d4c2dccc96842b6
marcosmeco789 <marcosmeco987@gmail.com>
2024-12-18 11:13:38 +0100

    18/12

--- EjerciciosPOO/Escritora.cs
+++ EjerciciosPOO/Escritora.cs
@@ -13,6 +13,27 @@ namespace EjerciciosPOO
         {
         }
 
+        public void Write(Empleado emp)
+        {
+            base.Write(emp.Nombre);
+            base.Write(emp.Apellidos);
+            base.Write(emp.Edad);
+            base.Write(emp.Dni);
+            base.Write(emp.Salario);
+            base.Write(emp.Telefono);
+        }
+
+        public void Write(Directivo dir)
+        {
+            base.Write(dir.Nombre);
+            base.Write(dir.Apellidos);
+            base.Write(dir.Edad);
+            base.Write(dir.Dni);
+            base.Write(dir.Departamento);
+            base.Write(dir.Personas);
+        }
+
+
    
     }
 }
--- EjerciciosPOO/Lectora.cs
+++ EjerciciosPOO/Lectora.cs
@@ -12,5 +12,34 @@ namespace EjerciciosPOO
         public Lectora(Stream str) : base(str)
         {
         }
+
+        public Empleado ReadEmpleado()
+        {
+
+            string nombre = ReadString();
+            string apellidos = ReadString();
+            int edad = ReadInt32();
+            string dni = ReadString();
+            int salario = ReadInt32();
+            string telefono = ReadString();
+
+
+            return new Empleado(nombre, apellidos, edad, dni, salario, telefono);
+        }
+
+        public Directivo ReadDirectivo()
+        {
+
+
+            string nombre = ReadString();
+            string apellidos = ReadString();
+            int edad = ReadInt32();
+            string dni = ReadString();
+            double salario = ReadDouble();
+            string departamento = ReadString();
+            int personas = ReadInt32();
+
+            return new Directivo(nombre, apellidos, edad, dni, departamento, personas);
+        }
     }
 }
